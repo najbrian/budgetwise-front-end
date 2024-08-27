9ae854bb467c4016e3b4670b43e539faa0bf2e2a
najbrian <najbrian@gmail.com>
2024-08-27 09:35:25 -0700

    created theme for page

--- src/components/BudgetList/BudgetList.jsx
+++ src/components/BudgetList/BudgetList.jsx
@@ -6,6 +6,12 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 
 const BudgetList = (props) => {
@@ -25,8 +31,8 @@ const BudgetList = (props) => {
                 <ListItem disablePadding>
                   <ListItemButton>
                     <ListItemText
-                    primary={`Budget Name: ${budget.name}`}
-                    secondary={`Budget Amount: $${budget.amount}`} />
+                      primary={`Budget Name: ${budget.name}`}
+                      secondary={`Budget Amount: $${budget.amount}`} />
                   </ListItemButton>
                 </ListItem>
               </Link>
@@ -38,7 +44,15 @@ const BudgetList = (props) => {
           <h2>Total Budget: {totalBudget}</h2>
         </div>
       </StyledBudgetListDiv>
-      <Link to={'/budgets/new'}><p>Add New Budget</p></Link>
+
+      <Link
+        to={'/budgets/new'}
+      >
+        <Fab color="primary" variant="extended">
+        <AddIcon sx={{ mr: 1 }} />
+        Add New Budget
+      </Fab>
+      </Link>
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Poppins", sans-serif'
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

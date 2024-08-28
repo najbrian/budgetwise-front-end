879e40bef6288a1658daf01d7692e7c448041ecd
najbrian <najbrian@gmail.com>
2024-08-28 14:32:14 -0700

    completed expense details page styling

--- src/components/ExpenseDetails/ExpenseDetails.jsx
+++ src/components/ExpenseDetails/ExpenseDetails.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react"
 import { useParams, useNavigate, Link } from "react-router-dom"
 import * as budgetService from '../../services/budgetService'
-import { } from './style'
+import { StyledLink, StyledSection, StyledBudgetButtonDiv } from './style'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemButton from '@mui/material/ListItemButton';
 
 const ExpenseDetails = (props) => {
   const { budgetId, expenseId } = useParams()
@@ -53,39 +53,56 @@ const ExpenseDetails = (props) => {
         <p>Created on {new Date(expense.createdAt).toLocaleDateString()} at {new Date(expense.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
         <p>Expense Type: {expense.type}</p>
         <p>Expense Amount: {expense.amount}</p>
-        <button onClick={() => { navigate(`/budgets/${budgetId}/expenses/${expenseId}/edit`) }}>Edit</button>
-        <button onClick={() => { props.handleDeleteExpense(budgetId, expenseId) }}>Delete</button>
-        <Link to={`/budgets/${budgetId}`}>Go Back</Link>
+        <StyledBudgetButtonDiv>
+          <ButtonGroup
+            disableElevation
+            variant="contained"
+            aria-label="Disabled button group"
+          >
+            <Button
+              onClick={() => { navigate(`/budgets/${budgetId}/expenses/${expenseId}/edit`) }}
+              sx={{
+                bgcolor: 'rgb(67,146,138)',
+                color: 'rgb(232, 241, 220)',
+                '&:hover': {
+                }
+              }}
+            >
+              Edit
+            </Button>
+            <Button
+              onClick={() => { props.handleDeleteExpense(budgetId, expenseId) }}
+              sx={{
+                bgcolor: 'rgb(67,146,138)',
+                color: 'rgb(232, 241, 220)',
+                '&:hover': {
+                }
+              }}
+            >
+              Delete
+            </Button>
+          </ButtonGroup>
+          <Link to={`/budgets/${budgetId}`}>Go Back</Link>
+        </StyledBudgetButtonDiv>
       </header>
       <section>
         <h2>Expense Notes:</h2>
         {expense?.notes?.length
           ? <>
-               <List sx={{ width: '100%', maxWidth: 360, fontFamily: 'Poppins' }}>
-            {expense.notes.map(note => (
+            <List sx={{ width: '100%', maxWidth: 360, fontFamily: 'Poppins' }}>
+              {expense.notes.map(note => (
                 <ListItem disablePadding>
-                    <ListItemText
-                      primary={`Note: ${note.text}`}
-                      secondary={
-                          `Created: ${new Date(note.createdAt).toLocaleDateString()} at ${new Date(note.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`
-                      }
-                    />
+                  <ListItemText
+                    primary={`Note: ${note.text}`}
+                    secondary={
+                      `Created: ${new Date(note.createdAt).toLocaleDateString()} at ${new Date(note.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`
+                    }
+                  />
                 </ListItem>
 
 
-            ))}
-          </List>
-         
-            {/* <ul>
-              {expense.notes.map(note => {
-                return (
-                  <li key={note._id}>{note.owner.firstname}: {note.text}
-                    <p>created on {new Date(note.createdAt).toLocaleDateString()} at {new Date(note.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-                    <button onClick={() => handleDeleteNote(note._id)}>Delete</button>
-                  </li>
-                )
-              })}
-            </ul> */}
+              ))}
+            </List>
             {props.isFormOpen &&
               <Box
                 component="form"
@@ -138,32 +155,32 @@ const ExpenseDetails = (props) => {
               </>
               :
               <>
-              <Box
-                component="form"
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  '& .MuiTextField-root': { m: 1, width: '25ch' }
-                }}
-                noValidate
-                autoComplete="off"
-                onSubmit={handleSubmit}
-              >
-                <TextField
-                  multiline
-                  rows={4}
-                  label="Expense Note"
-                  variant="filled"
-                  required
-                  type="text"
-                  name="text"
-                  id="note-input"
-                  value={expense.text}
-                  onChange={handleChange}
-                />
-                <Button type="submit" variant="contained" size="medium">Submit</Button>
-              </Box>
+                <Box
+                  component="form"
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    '& .MuiTextField-root': { m: 1, width: '25ch' }
+                  }}
+                  noValidate
+                  autoComplete="off"
+                  onSubmit={handleSubmit}
+                >
+                  <TextField
+                    multiline
+                    rows={4}
+                    label="Expense Note"
+                    variant="filled"
+                    required
+                    type="text"
+                    name="text"
+                    id="note-input"
+                    value={expense.text}
+                    onChange={handleChange}
+                  />
+                  <Button type="submit" variant="contained" size="medium">Submit</Button>
+                </Box>
                 <Button
                   onClick={() => { props.setIsFormOpen(!props.isFormOpen) }}
                   variant="contained"
--- src/components/ExpenseDetails/style.js
+++ src/components/ExpenseDetails/style.js
@@ -1,6 +1,26 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+const StyledSection = styled.section`
+display: flex;
+justify-content: space-evenly;
+`
+
+const StyledLink = styled(Link)`
+text-decoration: none;
+color: black;
+`
+
+const StyledBudgetButtonDiv= styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+`
 
 export {
-
+  StyledSection,
+  StyledLink,
+  StyledBudgetButtonDiv
 }
